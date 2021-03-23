36e2dd2be0dd67204ff491867488cda7b9052d65
FlorianAlbert <florian21albert@gmx.de>
2021-03-23 14:28:31 +0100

    Added SignalR Hub for Inbound Messages

---
+++ Apollon.MUD.Prototype.Inbound.SignalR/InboundTextHub.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Apollon.MUD.Prototype.Inbound.SignalR
+{
+    public class InboundTextHub : Hub
+    {
+        public static ConcurrentDictionary<string, List<string>> ConnectedUsers = new ConcurrentDictionary<string, List<string>>();
+
+        public override async Task OnConnectedAsync()
+        {
+            Trace.TraceInformation("InboundTextHub started. ID: {0}", Context.ConnectionId);
+
+            // TODO: check Name
+            var userName = Context.User.Identity.Name;
+            
+            ConnectedUsers.TryGetValue(userName, out var existingUserConnectionIds);
+
+            existingUserConnectionIds ??= new List<string>();
+            
+            existingUserConnectionIds.Add(Context.ConnectionId);
+            
+            ConnectedUsers.TryAdd(userName, existingUserConnectionIds);
+
+            await base.OnConnectedAsync();
+        }
+
+        public async Task EnterDungeon(int dungeonId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, dungeonId.ToString());
+        }
+
+        public async Task LeaveDungeon(int dungeonId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, dungeonId.ToString());
+        }
+    }
+}
