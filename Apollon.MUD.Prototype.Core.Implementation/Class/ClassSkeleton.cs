5193a544be0084f7d0ac5b81cc9c541618a3c7b5
Etienne Zink <etienne-zink@gmx.de>
2021-03-25 09:36:27 +0100

    Added Race- and ClassSkeleton with Builder-Class to generate the object.

---
+++ Apollon.MUD.Prototype.Core.Implementation/Class/ClassSkeleton.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Apollon.MUD.Prototype.Core.Interfaces.Configuration.AvatarConfigs;
+
+namespace Apollon.MUD.Prototype.Core.Implementation.Class
+{
+    public class ClassSkeleton : IClass
+    {
+        public string Name { get; }
+        public int DefaultHealthMax { get; }
+        public int DefaultDamage { get; }
+        public int DefaultProtection { get; }
+
+        private ClassSkeleton(string Name, int DefaultHealthMax, int DefaultDamage, int DefaultProtection)
+        {
+            this.Name = Name;
+            this.DefaultHealthMax = DefaultHealthMax;
+            this.DefaultDamage = DefaultDamage;
+            this.DefaultProtection = DefaultProtection;
+        }
+
+        public class ClassBuilder
+        {
+            private string Name;
+            private int DefaultHealthMax;
+            private int DefaultDamage;
+            private int DefaultProtection;
+
+            public ClassSkeleton build()
+            {
+                return new ClassSkeleton(Name, DefaultHealthMax, DefaultDamage, DefaultProtection);
+            }
+
+            public ClassBuilder setName(string name)
+            {
+                Name = name;
+                return this;
+            }
+
+            public ClassBuilder setDefaultHealth(int defaultHealthMax)
+            {
+                DefaultHealthMax = defaultHealthMax;
+                return this;
+            }
+
+            public ClassBuilder setDefaultDamage(int defaultDamage)
+            {
+                DefaultDamage = defaultDamage;
+                return this;
+            }
+
+            public ClassBuilder setDefaultProtection(int defaultProtection)
+            {
+                DefaultProtection = defaultProtection;
+                return this;
+            }
+        }
+
+    }
+}
---
+++ Apollon.MUD.Prototype.Core.Implementation/Race/RaceSkeleton.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Apollon.MUD.Prototype.Core.Interfaces.Configuration.AvatarConfigs;
+
+namespace Apollon.MUD.Prototype.Core.Implementation.Race
+{
+    public class RaceSkeleton : IRace
+    {
+        public string Name { get; }
+        public int DefaultHealthMax { get; }
+        public int DefaultDamage { get; }
+        public int DefaultProtection { get; }
+
+        private RaceSkeleton(string Name, int DefaultHealthMax, int DefaultDamage, int DefaultProtection)
+        {
+            this.Name = Name;
+            this.DefaultHealthMax = DefaultHealthMax;
+            this.DefaultDamage = DefaultDamage;
+            this.DefaultProtection = DefaultProtection;
+        }
+
+        public class RaceBuilder
+        {
+            private string Name;
+            private int DefaultHealthMax;
+            private int DefaultDamage;
+            private int DefaultProtection;
+
+            public RaceSkeleton build()
+            {
+                return new RaceSkeleton(Name, DefaultHealthMax, DefaultDamage, DefaultProtection);
+            }
+
+            public RaceBuilder setName(string name)
+            {
+                Name = name;
+                return this;
+            }
+
+            public RaceBuilder setDefaultHealth(int defaultHealthMax)
+            {
+                DefaultHealthMax = defaultHealthMax;
+                return this;
+            }
+
+            public RaceBuilder setDefaultDamage(int defaultDamage)
+            {
+                DefaultDamage = defaultDamage;
+                return this;
+            }
+
+            public RaceBuilder setDefaultProtection(int defaultProtection)
+            {
+                DefaultProtection = defaultProtection;
+                return this;
+            }
+        }
+    }
+}
